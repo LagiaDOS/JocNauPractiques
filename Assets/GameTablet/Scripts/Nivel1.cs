0cac1a916a46120f4e5cffffa566fa2f69b70105
Pol Quintana <100130129+Polete-DAM@users.noreply.github.com>
2023-10-10 09:45:05 +0200

    Nivell 1 lletres arreglades

--- Assets/GameTablet/Scripts/Nivel1.cs
+++ Assets/GameTablet/Scripts/Nivel1.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Nivel1 : Niveles
 {
+    private int probLletra;
 
     void Update()
     {
@@ -14,6 +15,8 @@ public class Nivel1 : Niveles
             if (!Settings.gamePause)
             {
                 spwanLetrasNumros -= Time.deltaTime;
+                probLletra = Random.Range(0, 4);
+                // Debug.Log(probLletra,gameObject);
 
                 if (spwanLetrasNumros <= 0.0f)
                 {
@@ -31,12 +34,24 @@ public class Nivel1 : Niveles
     public override void spawn()
     {
         GameObject p = Instantiate(prefabNumerosLetras, new Vector3(Random.Range(-3.0f, 2.0f), 5, 0), Quaternion.identity) as GameObject;
-        p.GetComponent<SpriteRenderer>().sprite = spritesNumerosLetras[letraRandom];
-        p.gameObject.tag = spritesNumerosLetras[letraRandom].name;
+
+        switch (probLletra)
+        {
+            case 3:
+                p.GetComponent<SpriteRenderer>().sprite = spritesNumerosLetras[letraRandomAdivinar];
+                p.gameObject.tag = spritesNumerosLetras[letraRandomAdivinar].name;
+                break;
+            default:
+                letraRandom = Random.Range(0, 27);
+                p.GetComponent<SpriteRenderer>().sprite = spritesNumerosLetras[letraRandom];
+                p.gameObject.tag = spritesNumerosLetras[letraRandom].name;
+                break;
+        }
+    
     }
     public override void valorAdivinar()
     {
-        GameObject.Find("valorAdivinar").GetComponent<Image>().sprite= spritesNumerosLetras[letraRandom];
-        tagAdivinar = spritesNumerosLetras[letraRandom].name;
+        GameObject.Find("valorAdivinar").GetComponent<Image>().sprite= spritesNumerosLetras[letraRandomAdivinar];
+        tagAdivinar = spritesNumerosLetras[letraRandomAdivinar].name;
     }
 }
--- Assets/GameTablet/Scripts/Niveles.cs
+++ Assets/GameTablet/Scripts/Niveles.cs
@@ -8,7 +8,9 @@ abstract public class Niveles : MonoBehaviour, ConfigurationLevels
     public GameObject prefabAdivinar;
     public Sprite[] spritesNumerosLetras;
     public float spwanLetrasNumros = 1;
+    public int letraRandomAdivinar;
     public int letraRandom;
+    public int numeroRandomAdivinar;
     public int numeroRandom;
     public static string tagAdivinar;
     public static bool gameOver;
@@ -17,8 +19,8 @@ abstract public class Niveles : MonoBehaviour, ConfigurationLevels
         prefabNumerosLetras = Resources.Load("Prefabs/prefabNumLetras") as GameObject;
         prefabAdivinar = Resources.Load("Prefabs/prefabAdivinar") as GameObject;
         spritesNumerosLetras = Resources.LoadAll<Sprite>("Sprites/LetrasNumeros");
-        letraRandom = Random.Range(0, 27);
-        numeroRandom = Random.Range(28, 37);
+        letraRandomAdivinar = Random.Range(0, 27);
+        numeroRandomAdivinar = Random.Range(28, 37);
         valorAdivinar();
         gameOver = false;
         Settings.gamePause = false;
