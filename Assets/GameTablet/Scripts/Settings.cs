a76598cc95fb0ccbc6041ad2ac65857d1a968d35
LagiaDOS <rgrrtz@gmail.com>
2023-11-14 09:52:18 +0100

    treball

--- Assets/GameTablet/Scripts/Settings.cs
+++ Assets/GameTablet/Scripts/Settings.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Settings : MonoBehaviour {
-
-
     public GameObject panel;
     public GameObject buttonRight;
     public GameObject buttonLeft;
@@ -15,10 +13,6 @@ public class Settings : MonoBehaviour {
     public  void panelShowHide()
     {
         //pausa el joc, i mostra el menu de pausa
-        gamePause = !gamePause;
-        state = !state;
-        panel.gameObject.SetActive(state);
-        
     }
 
     public void salirMenu()
@@ -27,11 +21,59 @@ public class Settings : MonoBehaviour {
         ScoreScript.scoreValue = 0;
         SceneManager.LoadScene(0);
     }
+
+    public void salirMundo()
+    {
+        switch (PlayerPrefs.GetString("Nivel"))
+        {
+            case "Nivel1":
+                SceneManager.LoadScene(1);
+                break;
+            case "Nivel2":
+                SceneManager.LoadScene(1);
+                break;
+            case "Nivel3":
+                SceneManager.LoadScene(1);
+                break;
+
+            case "Nivel4":
+                SceneManager.LoadScene(2);
+                break;
+            case "Nivel5":
+                SceneManager.LoadScene(2);
+                break;
+            case "Nivel6":
+                SceneManager.LoadScene(2);
+                break;
+
+            case "Nivel7":
+                SceneManager.LoadScene(3);
+                break;
+            case "Nivel8":
+                SceneManager.LoadScene(3);
+                break;
+            case "Nivel9":
+                SceneManager.LoadScene(3);
+                break;
+
+            case "Nivel10":
+                SceneManager.LoadScene(4);
+                break;
+            case "Nivel11":
+                SceneManager.LoadScene(4);
+                break;
+            case "Nivel12":
+                SceneManager.LoadScene(4);
+                break;
+        }
+    }
+
     public void salirJuego()
     {
         //tanca el joc
         Application.Quit();
     }
+
     public void replayGame()
     {
         //reinicia el nivell
@@ -45,47 +87,52 @@ public class Settings : MonoBehaviour {
         {
             case "Nivel1":
                 PlayerPrefs.SetString("Nivel", "Nivel2");
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(5);
                 break;
             case "Nivel2":
                 PlayerPrefs.SetString("Nivel", "Nivel3");
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(5);
                 break;
             case "Nivel3":
                 PlayerPrefs.SetString("Nivel", "Nivel4");
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(5);
                 break;
             case "Nivel4":
                 PlayerPrefs.SetString("Nivel", "Nivel5");
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(5);
                 break;
             case "Nivel5":
                 PlayerPrefs.SetString("Nivel", "Nivel6");
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(5);
                 break;
             case "Nivel6":
                 PlayerPrefs.SetString("Nivel", "Nivel7");
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(5);
                 break;
             case "Nivel7":
                 PlayerPrefs.SetString("Nivel", "Nivel8");
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(5);
                 break;
             case "Nivel8":
                 PlayerPrefs.SetString("Nivel", "Nivel9");
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(5);
+                break;
+            case "Nivel9":
+                PlayerPrefs.SetString("Nivel", "Nivel10");
+                SceneManager.LoadScene(5);
                 break;
             case "Nivel10":
-                PlayerPrefs.SetString("Nivel", "Nivel10");
-                SceneManager.LoadScene(1);
+                PlayerPrefs.SetString("Nivel", "Nivel11");
+                SceneManager.LoadScene(5);
                 break;
             case "Nivel11":
                 PlayerPrefs.SetString("Nivel", "Nivel12");
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(5);
                 break;
             case "Nivel12":
-                //carregar escena de credits
-                
+                //cambiar a escena de credits
+                PlayerPrefs.SetString("Nivel", "Nivel1");
+                SceneManager.LoadScene(0);
                 break;
 
         }
