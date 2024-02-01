05271ee1ec8e83fa031f9f1a47db0cc14d027023
LagiaDOS <rgrrtz@gmail.com>
2024-02-01 12:28:25 +0100

    com
    com

--- Assets/GameTablet/Scripts/myTime.cs
+++ Assets/GameTablet/Scripts/myTime.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class myTime : MonoBehaviour {
     //Variables para el tiempo
-    public float meTime;       // -----> Tiempo que dura el juego
+    public static float meTime;       // -----> Tiempo que dura el juego
     private Text timerText;         // -----> Texto donde se guarda el tiempo
     public GameObject gameOver;
+
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +17,10 @@ public class myTime : MonoBehaviour {
         //depenent el nivell en el que estem, el temps disponible pot cambiar
         switch (PlayerPrefs.GetString("Nivel"))
         {
+            case "NivelTutorial":
+                meTime = 120f;
+                break;
+
             default:
                 meTime = 60f;
                 break;
@@ -26,7 +31,7 @@ public class myTime : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (!Niveles.gameOver && !Settings.gamePause)
+        if (!Niveles.gameOver && !Settings.gamePause && Niveles.tutorial == false)
         {
             //redueix el temps en 1 cada 1 segon
                 meTime -= Time.deltaTime;
